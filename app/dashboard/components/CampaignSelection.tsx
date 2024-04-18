4822f75f491dce24240b9c18d8aa485c6ee0614e
Tomcroms <thomaslaumonier@orange.fr>
2024-04-18 11:51:55 +0200

    remove newCampaignForm

--- app/dashboard/components/CampaignSelection.tsx
+++ app/dashboard/components/CampaignSelection.tsx
@@ -9,20 +9,19 @@ const CampaignSelection = () => {
 
     return (
         <div className="flex ml-10 bg-white">
-            <section className={`${isOverflowHidden ? 'h-14 overflow-hidden' : 'h-max'} w-32 flex flex-col gap-2 z-10 rounded-xl border shadow-md`}>
-                {campaigns?.map((item) => (
-                    <CampaignBlock
-                        key={item.id}
-                        campaign={item}
-                    />
-                ))}
+            <section className={`${isOverflowHidden ? 'h-14 overflow-hidden' : 'h-max'} flex rounded-xl border shadow-md`}>
+                <div className="w-32 flex flex-col gap-2 z-10">
+                    {campaigns?.map((item) => (
+                        <CampaignBlock
+                            key={item.id}
+                            campaign={item}
+                        />
+                    ))}
+                </div>
+                <button className="px-4 bg-blue-500 text-white rounded-xl ml-10 text-[14px] h-14 py-2 w-36" onClick={() => setIsOverflowHidden(!isOverflowHidden)}>
+                    {isOverflowHidden ? 'Show Campaigns' : 'Hide'}
+                </button>
             </section>
-            <button
-                className="px-4 bg-blue-500 text-white rounded-xl ml-10 text-[14px] h-14 py-2 w-36"
-                onClick={() => setIsOverflowHidden(!isOverflowHidden)}
-            >
-                {isOverflowHidden ? 'Show Campaigns' : 'Hide'}
-            </button>
         </div>
     )
 }
