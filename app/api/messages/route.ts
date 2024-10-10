4ea1a6ca6cb1d98149b3d712deb715e6081e7b78
Tomcroms <thomaslaumonier@orange.fr>
2024-10-10 10:24:30 +0200

    adding .env

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { message, image, conversationId } = body;
 
-    if (!currentUser?.id || !currentUser?.email) {
+    if (!currentUser || !currentUser.id || !currentUser.email)      {
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
@@ -93,7 +93,6 @@ export async function POST(request: Request) {
 
     return NextResponse.json(newMessage);
   } catch (error) {
-    console.log(error, 'ERROR_MESSAGES');
     console.error('Error creating new message:', error);
     return new NextResponse('Database error', { status: 500 });
   }
